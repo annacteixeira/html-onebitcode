96b6350e4c378f8dc27e66b18c52a9241359eec7
Anna Chandra <72108588+annacteixeira@users.noreply.github.com>
2023-09-08 22:03:40 -0300

    Create cadastro-de-recrutas.js

---
+++ cadastro-de-recrutas.js
@@ -0,0 +1,13 @@
+const name = prompt("Insira seu nome") 
+const lastName = prompt("Insira seu sobrenome") 
+const study = prompt("Qual é seu campo de estudo?") 
+const birthYear = prompt("Insira seu ano de nascimento") 
+const age = (2023-birthYear) 
+
+alert(    
+    "Recruta cadastrado com sucesso!\n" + 
+    "\nNome completo: " + name + " " + lastName + 
+    "\nCampo de estudo: " + study +  
+    "\nIdade: " + age 
+
+)  // O COMANDO \n SERVE PARA PULAR LINHAS. SIMILAR A <br> DO HTML 
